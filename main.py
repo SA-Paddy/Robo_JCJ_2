d9b14e26b3588ff93903f2af600f9bca3ca2c0fa
SA-Paddy <Johannes.hearn@icloud.com>
2024-04-19 21:50:06 +0100

    INserted progress bar, inserted some error handling and message box.

--- main.py
+++ main.py
@@ -40,18 +40,27 @@ import math
 import time
 
 #Create Required Variables
-
+global ip_address
+ip_address=''
+global robot_connected
+robot_connected = False
+global robot
+robot = NiryoOneClient()
+global com_ports
+global selected_com_port
+global selected_baudrate
 
 #Create Functions
 def f_fetch_ip():
 
     def ip_save():
         global ip_address
+
         ip_address = ip_entry_box.get()
         ip_window.destroy()
 
         i_ip_address = tkinter.Label(info_frame, text=ip_address)
-        i_ip_address.grid(row=0, column=1, padx=(20, 30), pady=(20, 5))
+        i_ip_address.grid(row=0, column=1, padx=(20, 30), pady=(10, 5))
 
 
     default_ip="192.168.1."
@@ -75,64 +84,97 @@ def f_fetch_ip():
 def f_fetch_port():
     global com_ports
     global selected_com_port
+    global selected_baudrate
     com_ports = serial.tools.list_ports.comports()
 
     def com_save():
+        global selected_baudrate
+        global selected_com_port
         selected_port_value = selected_port.get()
         selected_com_port = selected_port_value.split()[0]
+        selected_baudrate = int(com_baud_entry.get())
         com_window.destroy()
 
         i_com_port = tkinter.Label(info_frame, text=selected_com_port)
         i_com_port.grid(row=1, column=1, padx=(20, 30), pady=(5, 5))
+        i_baudrate = tkinter.Label(info_frame, text=selected_baudrate)
+        i_baudrate.grid(row=2, column=1, padx=(20, 30), pady=(5, 5))
 
     com_window = tkinter.Toplevel()
     com_window.title("Arduino COM PORT")
     com_window.iconbitmap('robot.ico')
-    com_window.geometry('410x100')
+    com_window.geometry('420x120')
 
     #create a string variable that we can edit
     selected_port = tkinter.StringVar(com_window)
     selected_port.set(com_ports[0][0])
 
+    # Create a default baudrate value
+    default_baud = '9600'
+
     com_select_label = tkinter.Label(com_window, text='Select the COM Port: ')
-    com_select_label.grid(row=0, column=0, padx=(10, 5), pady=10)
+    com_select_label.grid(row=0, column=0, padx=(10, 5), pady=(10, 5))
+
+    com_baud_label = tkinter.Label(com_window, text='Enter applicable Baudrate: ')
+    com_baud_label.grid(row=1, column=0, padx=(10, 5), pady=(5, 5))
 
     com_options = ["{} - {}".format(port[0], port[1]) for port in com_ports]
     com_options_menu = tkinter.OptionMenu(com_window, selected_port, *com_options)
-    com_options_menu.grid(row=0, column=1, padx=(5, 10), pady=10)
+    com_options_menu.grid(row=0, column=1, padx=(5, 10), pady=(10, 5))
+
+    com_baud_entry = tkinter.Entry(com_window)
+    com_baud_entry.insert(tkinter.END, default_baud)
+    com_baud_entry.grid(row=1, column=1, padx=(5, 10), pady=(5, 5))
 
     com_port_save_btn = tkinter.Button(com_window, text="Save and Close", command=com_save)
-    com_port_save_btn.grid(row=1, column=1, padx=(20, 30), pady=(20, 5))
+    com_port_save_btn.grid(row=2, column=1, padx=(20, 30), pady=(5, 10))
 
 
     com_window.mainloop()
 
 def f_connect_attempt():
+
+    global ip_address
     global robot_connected
-    global robot
-    robot_connected = False
-    robot = NiryoOneClient()
+
     sleep_joints = [0.0, 0.55, -1.2, 0.0215024563846, -0.296705973, -0.070860367631]
 
     try:
+        update_message_box('Attempting to connect to Niryo One Robot')
         robot.connect(ip_address)
+        robot.get_hardware_status()
         robot_connected = True
-        #remove_tools_message = tkinter.messagebox.askokcancel(title='Remove Tooling', message='Please ensure all tooling'
-                                                                                              #' has been removed from'
-                                                                                              #' the TCP before clicking '
-                                                                                              #' ok. Failure to do so'
-                                                                                              #' will result in damage'
-                                                                                              #' to the robotic arm.',
-                                                              #icon=Warning)
-        #if remove_tools_message:
-        robot.calibrate(CalibrateMode.AUTO)
-        #robot.move_pose(-.0, -0.44, 0.15, -0.041, 0.758, -1.563)
-        robot.move_pose(0.15, 0., 0.2, -0.041, 0.758, -1.563)
-        time.sleep(1)
-        robot.move_joints(*sleep_joints)
-
     except Exception as e:
         robot_connected = False
+        print(f'Your connection attempt was to ip {ip_address}')
+        tkinter.messagebox.showerror(title='Failed to connect', message=f'Connection to to the one Niryo robot failed.'
+                                                                        'Please try again. Your connection attempt was'
+                                                                        f' to ip {ip_address}')
+
+    if robot_connected:
+        remove_tools_message = tkinter.messagebox.askokcancel(title='Remove Tooling', message='Please ensure all tooling'
+                                                                                              ' has been removed from'
+                                                                                              ' the TCP before clicking '
+                                                                                              ' ok. Failure to do so'
+                                                                                              ' will result in damage'
+                                                                                              ' to the robotic arm.',
+                                                              icon='warning')
+
+        if remove_tools_message:
+            try:
+                update_message_box('Checking if calibration is required')
+                robot.calibrate(CalibrateMode.AUTO)
+                update_message_box('Telling robot to nod to confirm to user that connection succesful')
+                #robot.move_pose(-.0, -0.44, 0.15, -0.041, 0.758, -1.563)
+                robot.move_pose(0.15, 0., 0.2, -0.041, 0.758, -1.563)
+                time.sleep(1)
+                robot.move_joints(*sleep_joints)
+            except Exception as e:
+                print('Just NOPE')
+        else:
+            tkinter.messagebox.showinfo(title='Abort Connection', message='Aborting Connection Attempt until '
+                                                                          'confirmation of Tooling removal.')
+
 
 
 def f_set_test():
@@ -154,13 +196,13 @@ def f_set_test():
         set_test_window.destroy()
 
         i_width = tkinter.Label(info_frame, text=width_mm)
-        i_width.grid(row=2, column=1, padx=(20, 30), pady=(5, 5))
+        i_width.grid(row=3, column=1, padx=(20, 30), pady=(5, 5))
 
         i_length = tkinter.Label(info_frame, text=length_mm)
-        i_length.grid(row=3, column=1, padx=(20, 30), pady=(5, 5))
+        i_length.grid(row=4, column=1, padx=(20, 30), pady=(5, 5))
 
         i_resolution = tkinter.Label(info_frame, text=resolution)
-        i_resolution.grid(row=4, column=1, padx=(20, 30), pady=(5, 5))
+        i_resolution.grid(row=5, column=1, padx=(20, 30), pady=(5, 5))
 
         generate_test_data(width_mm, length_mm, resolution)
 
@@ -253,7 +295,7 @@ def generate_test_data(width_mm, length_mm, resolution):
         ax.set_aspect('equal', 'box')
         ax.grid(True)
         ax.scatter(*zip(*coordinates), color='red', s=8)
-        ax.set_title('Porposed Test')
+        ax.set_title('Proposed Test')
         ax.set_ylabel('Length (y) Coordinates')
         ax.set_xlabel('Width (x) Coordinates')
 
@@ -265,12 +307,11 @@ def generate_test_data(width_mm, length_mm, resolution):
         toolbar.update()
         canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 
-        
-
-
-
+def update_progress(prog_val):
+    progress_bar['value'] = prog_val
 
 def f_run_test():
+    update_message_box('Blar Blar')
     return
 
 def f_save_test():
@@ -285,6 +326,7 @@ def f_close():
                                                                                            ' connection established'
                                                                                            ' to close.')
     else:
+        update_message_box('Closing down connection to Niryo One robot')
         sleep_joints = [0.0, 0.55, -1.2, 0.0215024563846, -0.296705973, -0.070860367631]
         # So we send the robot to the sleep_joints position
         robot.move_joints(*sleep_joints)
@@ -292,6 +334,22 @@ def f_close():
         robot.set_learning_mode(True)
         # We disconnect from the robot
         robot.quit()
+
+def update_message_box(message):
+    # Enable the Text widget to update its contents
+    message_print_box.config(state='normal')
+    # Insert the new message at the end of the Text widget
+    message_print_box.insert(tkinter.END, message + '\n')
+    # Delete the first line of message
+    num_lines = int(message_print_box.index('end-1c').split('.')[0])
+    # If the number of lines exceeds 4, delete the excess lines
+    if num_lines > 5:
+        delete_line = num_lines - 5
+        message_print_box.delete('1.0', f'{delete_line}.0')
+    # Disable the Text widget to prevent user input
+    # Disable the Text widget to prevent user input
+    message_print_box.config(state='disabled')
+
 
 #Create the Root Window
 root = Tk()
@@ -342,19 +400,31 @@ m_close.grid(row=7, column=0, pady=(5, 20), padx=20)
 
 #info window frame
 robo_ip_label = tkinter.Label(info_frame, text='Current Selected IP Address: ')
-robo_ip_label.grid(row=0, column=0, padx=(30, 20), pady=(20, 5))
+robo_ip_label.grid(row=0, column=0, padx=(30, 20), pady=(10, 5))
 
 arduino_com_label = tkinter.Label(info_frame, text='Current Selected COM Port: ')
 arduino_com_label.grid(row=1, column=0, padx=(30, 20), pady=(5, 5))
 
+arduino_baudrate_label = tkinter.Label(info_frame, text='COM port Baudrate: ')
+arduino_baudrate_label.grid(row=2, column=0, padx=(30, 20), pady=(5, 5))
+
 piece_width_label = tkinter.Label(info_frame, text='Piece Width in mm: ')
-piece_width_label.grid(row=2, column=0, padx=(30, 20), pady=(5, 5))
+piece_width_label.grid(row=3, column=0, padx=(30, 20), pady=(5, 5))
 
 piece_length_label = tkinter.Label(info_frame, text='Piece Length in mm: ')
-piece_length_label.grid(row=3, column=0, padx=(30, 20), pady=(5, 5))
+piece_length_label.grid(row=4, column=0, padx=(30, 20), pady=(5, 5))
 
 piece_resolution_label = tkinter.Label(info_frame, text='Selected Resolution (test points): ')
-piece_resolution_label.grid(row=4, column=0, padx=(30, 20), pady=(5, 5))
+piece_resolution_label.grid(row=5, column=0, padx=(30, 20), pady=(5, 5))
 
+progress_label = tkinter.Label(info_frame, text='Test progress: ')
+progress_label.grid(row=6, column=0, padx=(30, 20), pady=(5, 5))
+
+progress_bar = tkinter.ttk.Progressbar(info_frame, orient='horizontal', length=200, mode='determinate')
+progress_bar.grid(row=6, column=1, padx=(20, 30), pady=(5, 5))
+
+message_print_box = tkinter.Text(info_frame, height=5, width=53)
+message_print_box.grid(row=7, column=0, columnspan=2, padx=(30, 30), pady=(5, 10))
+message_print_box.config(state='disabled')
 
 root.mainloop()
